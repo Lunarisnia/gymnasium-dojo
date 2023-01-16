07193642e48bab82aa15d92ae75a2cbf83905b71
Rio <r.arswendo.r@gmail.com>
2023-01-16 18:25:30 +0700

    add progress

--- intro/main.py
+++ intro/main.py
@@ -1,3 +1,10 @@
 from src.intro import main
 
-main()
+# main()
+
+from collections import defaultdict
+import numpy as np
+# from tqdm import tqdm
+
+foo = defaultdict(lambda: np.zeros(4))
+tup = (1, 20, False)
--- intro/src/blackjack.py
+++ intro/src/blackjack.py
@@ -1,14 +1,28 @@
 
 # Task: Solving Blackjack with Q-Learning
+# Q-Learning learning adalah salah satu algoritma Reinforcement Learning
+# yang berjalan menggunakan sebuah Q-Table, Q-Table adalah kumpulan
+# state dari hasil observasi yang dijadikan key dan berisi opsi pilihan terbaik untuk kondisi tersebut
+# Proses learning pada Q-Learning pada dasarnya hanya proses untuk membuat Q-Table tersebut.
 
 from __future__ import annotations
 
+# For creating a dictionary with custom default response instead of the default KeyError
 from collections import defaultdict
 
+# Plotting a data
 import matplotlib.pyplot as plt
+
+# Numpy
 import numpy as np
+
+# For Easier good looking data plotting
 import seaborn as sns
+
+# For Creating shapes easily, also why is this called Artist?
 from matplotlib.patches import Patch
+
+# For creating progress bar
 from tqdm import tqdm
 
 import gymnasium as gym
@@ -58,13 +72,23 @@ class BlackjackAgent:
             discount_factor: The discount factor for computing the Q-Value
         """
         self.q_values = defaultdict(lambda: np.zeros(env.action_space.n))
+        # q_values = {
+        #     (13, 6, False): [0. 0.01],
+        #     (20, 16, False): [-0.01 0.],
+        # }
 
         self.lr = learning_rate
+        # lr = 0.01
+        
         self.discount_factor = discount_factor
+        # discount_factor = 0.95
 
         self.epsilon = intial_epsilon
+        # epsilon = 0.99999
         self.epsilon_decay = epsilon_decay
+        # epsilon_decay = 1e-5
         self.final_epsilon = final_epsilon
+        # final_epsilon = 0.1
 
         self.training_error = []
 
@@ -90,20 +114,48 @@ class BlackjackAgent:
         next_obs: tuple[int, int, bool],
     ):
         """Updates the Q-value of an action."""
+        # q_values = {
+        #   (13, 6, False): [0. 0.01],
+        #   (20, 16, False): [-0.01 0.],
+        # }
+
+        # lr = 0.01
+        # obs = (2, 10, False)
+        # next_obs = (12, 11, False)
+        # reward = 1.5
+        # action = 1
+        # terminated = False
+        # truncated = False
+        # info = {}
+        # discount_factor = 0.95
         future_q_value = (not terminated) * np.max(self.q_values[next_obs])
+        # q_values[(12, 11, False)] = [0. 0.] = 0.
+        # future_q_value = 1 * 0 = 0
+
+        # Why do we need to subtract the future q-value with the old one?
         temporal_difference = (
             reward + self.discount_factor *
             future_q_value - self.q_values[obs][action]
         )
+        # PEMDAS: Parenthesis, Exponential, Multiplication, Addition, Subtraction
+        # q_values[(2, 10, False)][0] = 0.0
+        # temporal_difference = 1.5 + 0.95 * 0 - 0 = -1
 
         self.q_values[obs][action] = (
             self.q_values[obs][action] + self.lr * temporal_difference
         )
+        # q_values[(20, 16, False)][0] = 0.0
+        # q_values[(20, 16, False)][0] = 0.0 + 0.01 * -1.0 = 0.0 - 0.01 = -0.01
+        
         self.training_error.append(temporal_difference)
+        # training_error = [1.0]
+        # append = [1.0, -1]
 
     def decay_epsilon(self):
         self.epsilon = max(self.final_epsilon,
                            self.epsilon - self.epsilon_decay)
+        # epsilon = max(0.1, (1.0 - 1e-5)) 
+        # = max(0.1, 0.99999) = 0.99999
 
 
 # Agent is done time to train
@@ -112,23 +164,90 @@ n_episodes = 100_000
 start_epsilon = 1.0
 # reduce the exploration over time
 epsilon_decay = start_epsilon / (n_episodes / 2)
+# epsilon_decay = 1e-5
 final_epsilon = 0.1
-
 agent = BlackjackAgent(learning_rate=learning_rate,
                        intial_epsilon=start_epsilon,
                        epsilon_decay=epsilon_decay,
                        final_epsilon=final_epsilon)
  
 # Training
+
+"""
+========
+EPISODE 1
+========
+
+
+1st Iteration
+
+q_values = {
+   (13, 6, False): [0. 0.01]
+}
+training_error = [1.0]
+
+lr = 0.01
+obs = (13, 6, False)
+next_obs = (20, 16, False)
+reward = 1.0
+action = 1
+terminated = False
+truncated = False
+info = {}
+discount_factor = 0.95
+
+future_q_value = 1 * 0 = 0
+temporal_difference = 1 + 0.95 * 0 - 0 = 1.0
+q_values[(13, 6, False)][1] = 0.0 + 0.01 * 1.0 = 0.0 + 0.01 = 0.01
+training_error = [] = [1.0]
+
+
+2nd Iteration
+q_values = {
+  (13, 6, False): [0. 0.01],
+  (20, 16, False): [-0.01 0.],
+}
+training_error = [1.0, -1]
+
+lr = 0.01
+obs = (20, 16, False)
+next_obs = (29, 20, False)
+reward = -1.0
+action = 0
+terminated = True
+truncated = False
+info = {}
+discount_factor = 0.95
+
+future_q_value = 0 * 0 = 0
+temporal_difference = -1 + 0.95 * 0 - 0 = -1
+
+
+epsilon = 0.99999
+
+
+
+=======
+EPISODE 2
+=======
+"""
+
+# Todo: Create an interactive menu to view value during training
+
+# Record cumulative rewards and episode lengths
 env = gym.wrappers.RecordEpisodeStatistics(env, deque_size=n_episodes)
 for episode in tqdm(range(n_episodes)):
     obs, info = env.reset()
+    # obs = (2, 10, False)
     done = False
 
     # play one episode
     while not done:
         action = agent.get_action(obs)
+        # 1st iteration: Random sample = 1
+
         next_obs, reward, terminated, truncated, info = env.step(action)
+        
 
         # update the agent
         agent.update(obs, action, reward, terminated, next_obs)
@@ -137,6 +256,8 @@ for episode in tqdm(range(n_episodes)):
         done = terminated or truncated
         obs = next_obs
 
+    # Why is it only updated once the episode is over?
+    # Maybe because we want to try a single epsilon for an episode to see how effective that is
     agent.decay_epsilon()
 
 # Todo: Learn how the code actually works
